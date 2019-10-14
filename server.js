e7074b0c3b75c79f35608471a64418787f70953c
Nathaniel <swallowtailjig@gmail.com>
2019-10-14 11:33:14 -0400

    test

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 /**
  * Do not change this file
  */
+const express = require("express");
+const app = express();
 
- 
- 
+app.use(express.static('.'));
+app.use(express.json());
+
+app.listen('8080', function(){console.log("listening at port 8080...")});
